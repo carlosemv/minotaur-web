cefa13c00c140baf02513331efba34b77fcc533a
Carlos Vieira <vieiramecarlos@gmail.com>
2019-08-16 17:03:11 -0300

    Refactor entities

--- sketch.js
+++ sketch.js
@@ -5,9 +5,21 @@ var pov = undefined;
 
 var player = undefined;
 
+var TileType = {"none":1, "basic":2, "wall":3, "border":4};
+Object.freeze(TileType);
+
+var EnemyType = {"simple":1};
+Object.freeze(EnemyType);
+
+var enemySprites = {};
+
 function preload() {
   // randomSeed(2);
 }
+
+// function keyPressed() {
+//   console.log(keyCode);
+// }
 
 function setup() {
   tile = { w: 50, h: 50, c: 3};
@@ -20,24 +32,24 @@ function setup() {
 
   createCanvas(pov.w*tile.w, pov.h*tile.h);
 
-  player = new Player();
-
   var mapSz = 256;
   map = new Map(mapSz);
 
-  var enemyImg = loadImage("assets/enemy.png");
+  player = new Player();
+  map.insert(1, 1, player);
+
+  enemies = new Array();
+  enemySprites[EnemyType.simple] = loadImage("assets/enemy.png");
+
   for (var i = 0; i < mapSz*10; ++i) {
-    var enemy = createSprite(0, 0,
-      tile.w, tile.h);
-    enemy.addImage(enemyImg);
+    var enemy = new Enemy(EnemyType.simple);
+    enemies.push(enemy);
     do {
       var x = Math.floor(random(mapSz));
       var y = Math.floor(random(mapSz));
     } while(!map.get(x, y).available());
     map.insert(x, y, enemy);
   }
-  console.log("done spawning");
-
 }
 
 function draw() {
@@ -45,65 +57,22 @@ function draw() {
 
   player.move();
 
-  stroke(180, 0, 0);
-
   for (var dx = -pov.hw; dx <= pov.hw; dx++) {
     for (var dy = -pov.hh; dy <= pov.hh; dy++) {
-      var entities = false;
-      var t;
-      if (dx == 0 && dy == 0) {
-        strokeWeight(3);
-        fill(100, 100, 255);
-      } else {
-        t = map.get(player.x+dx, player.y+dy);
-        if (t.type === TileType.basic) {
-          strokeWeight(3);
-          if (t.isOccupied()) {
-            fill(255, 100, 100);
-            entities = true;
-          } else {
-            fill(0xdd);
-          }
-        } else if (t.type === TileType.wall) {
-          strokeWeight(3);
-          fill(0x77)
-        } else if (t.type === TileType.border) {
-          strokeWeight(3);
-          fill(0);
-        } else {
-          strokeWeight(0);
-          fill(0);
-        }
-      }
-      rect((dx+pov.hw)*tile.w, (dy+pov.hh)*tile.h,
-        tile.w, tile.h, tile.c);
-      if (entities)
-        t.drawEntities(dx+pov.hw, dy+pov.hh);
+      var t = map.get(player.x+dx, player.y+dy);
+      t.draw(dx+pov.hw, dy+pov.hh);
     }
   }
-
-  drawSprite(player.sprite);
 }
-
-var TileType = {"none":1, "basic":2, "wall":3, "border":4}
-Object.freeze(TileType);
 
 class Tile {
   constructor(type) {
     this.type = type;
-    this.entities = new Array();
-  }
-
-  drawEntities(x, y) {
-    for (var i = 0; i < this.entities.length; i++) {
-      this.entities[i].position.x = x*tile.w + tile.hw;
-      this.entities[i].position.y = y*tile.h + tile.hh;
-      drawSprite(this.entities[i]);
-    }
+    this.entity = null;
   }
 
   isOccupied() {
-    return (this.entities.length > 0);
+    return (this.entity != null);
   }
 
   available() {
@@ -111,17 +80,53 @@ class Tile {
   }
 
   insert(entity) {
-    this.entities.push(entity);
+    this.entity = entity;
+  }
+
+  evict() {
+    this.entity = null;
+  }
+
+  draw(x, y) {
+    stroke(180, 0, 0);
+    switch (this.type) {
+      case TileType.basic:
+        strokeWeight(3);
+        fill(0xdd);
+        break;
+      case TileType.wall:
+        strokeWeight(3);
+        fill(0x77)
+        break;
+      case TileType.border:
+        strokeWeight(3);
+        fill(0);
+        break;
+      default:
+        strokeWeight(0);
+        fill(0);
+    }
+    rect(x*tile.w, y*tile.h,
+      tile.w, tile.h, tile.c);
+    if (this.isOccupied())
+      this.entity.draw(x, y);
   }
 }
 
+class Enemy {
+  constructor(type) {
+    this.sprite = enemySprites[type];
+  }
+
+  draw(x, y) {
+    imageMode(CENTER);
+    image(this.sprite, x*tile.w+tile.hw, y*tile.h+tile.hh);
+  }
+}
 
 class Player {
   constructor() {
-    this.sprite = createSprite(width/2, height/2,
-      tile.w, tile.h);
-    var playerImg = loadImage("assets/player.png");
-    this.sprite.addImage(playerImg);
+    this.sprite = loadImage("assets/player.png");
     this.x = 1;
     this.y = 1;
   }
@@ -130,19 +135,28 @@ class Player {
     var ox = this.x;
     var oy = this.y;
 
-    if (keyWentDown(LEFT_ARROW) && player.x > 0)
+    if (keyWentDown(35) || keyWentDown(36) || keyWentDown(37))
       this.x -= 1;
-    if (keyWentDown(RIGHT_ARROW) && player.x < map.size)
+    if (keyWentDown(33) || keyWentDown(34) || keyWentDown(39))
       this.x += 1;
-    if (keyWentDown(UP_ARROW) && player.y > 0)
+    if (keyWentDown(33) || keyWentDown(36) || keyWentDown(38))
       this.y -= 1;
-    if (keyWentDown(DOWN_ARROW) && player.y < map.size)
+    if (keyWentDown(34) || keyWentDown(35) || keyWentDown(40))
       this.y += 1;
 
-    if (map.get(this.x, this.y).type != TileType.basic) {
+    if (map.get(this.x, this.y).type != TileType.basic
+        || map.isOut(this.x, this.y)) {
       this.x = ox;
       this.y = oy;
+    } else {
+      map.evict(ox, oy);
+      map.insert(this.x, this.y, this);
     }
+  }
+
+  draw() {
+    imageMode(CENTER);
+    image(this.sprite, width/2, height/2);
   }
 }
 
@@ -167,10 +181,13 @@ class Map {
     return (x == 0 || y == 0 || x == this.size-1 || y == this.size-1);
   }
 
+  isOut(x, y) {
+    return (x < 0 || x > this.size || y < 0 || y > this.size);
+  }
+
   key(x, y) {
-    if (x < 0 || x > this.size || y < 0 || y > this.size) {
+    if (this.isOut(x, y))
       return undefined;
-    }
 
     return x * this.size + y;
   }
@@ -188,5 +205,9 @@ class Map {
 
   insert(x, y, entity) {
     this.get(x, y).insert(entity);
+  }
+
+  evict(x, y) {
+    this.get(x, y).evict();
   }
 }
